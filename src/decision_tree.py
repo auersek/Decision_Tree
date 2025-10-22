906a48f1d7cefa7a3c0a739c75c4bfcc66930b96
auersek <122909164+auersek@users.noreply.github.com>
2025-10-22 18:53:11 +0100

    basic empty functions added

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -1,3 +1,20 @@
+import numpy as np
+
+def load_data_from_file():
+    return 0
+
+
+def decision_tree_learning(mat_A, depth):
+    return 0
+
+
+
+if __name__ == '__main__':
+
+    decision_tree_learning()
+
+
+
 
 
 
