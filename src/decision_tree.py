a2c2a92e65d69c4df7a42965968d8ec2da5073eb
auersek <122909164+auersek@users.noreply.github.com>
2025-10-22 19:13:23 +0100

    realtive path used

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -1,13 +1,10 @@
 # import numpy as np
 # import matplotlib
 
-
 def load_data_from_file(file_name):
     file = open(file_name)
     print(file.read())
     return 0
-
-
 
 def decision_tree_learning(mat_A, depth):
     return 0
@@ -16,10 +13,9 @@ def evaluate(test_db, trained_tree):
     return 0
 
 
-
 if __name__ == '__main__':
 
-    load_data_from_file("/Users/aurelersek/Desktop/Decision_Trees/src/For_60012/wifi_db/clean_dataset.txt")
+    load_data_from_file("src/For_60012/wifi_db/clean_dataset.txt")
 
 
 
