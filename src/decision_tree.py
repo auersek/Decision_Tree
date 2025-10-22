2456e98b738a0aa59913fcc779ec184d4c331567
auersek <122909164+auersek@users.noreply.github.com>
2025-10-22 18:59:52 +0100

    add basic functions

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -1,10 +1,16 @@
 import numpy as np
+import matplotlib
+
 
 def load_data_from_file():
     return 0
 
 
+
 def decision_tree_learning(mat_A, depth):
+    return 0
+
+def evaluate(test_db, trained_tree):
     return 0
 
 
