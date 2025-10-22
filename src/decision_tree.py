bf612359e7e127fd63e64d7b4aa634e920f9e40c
auersek <122909164+auersek@users.noreply.github.com>
2025-10-22 19:24:56 +0100

    data load added

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -1,10 +1,18 @@
-# import numpy as np
-# import matplotlib
+import numpy as np
+import matplotlib
+
+# class Nodes:
+    
+
 
 def load_data_from_file(file_name):
-    file = open(file_name)
-    print(file.read())
-    return 0
+    data = []
+    with open(file_name, "r") as file:
+        for line in file:
+            row = list(map(int, line.split()))
+            data.append(row)
+    print(data)
+    return data
 
 def decision_tree_learning(mat_A, depth):
     return 0
@@ -14,6 +22,8 @@ def evaluate(test_db, trained_tree):
 
 
 if __name__ == '__main__':
+
+    wifi_dict = {}
 
     load_data_from_file("src/For_60012/wifi_db/clean_dataset.txt")
 
